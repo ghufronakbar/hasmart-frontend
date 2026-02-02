7b557e40a8814d88269e506db9c359176eb1517e
lanstheprodigy <alan.040219.gam@gmail.com>
2026-02-02 18:43:09 +0700

    add cors

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,27 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   async rewrites() {
     return [
       {
         source: "/api/:path*",
         destination: `${process.env.BACKEND_URL}/api/:path*`,
+      },
+    ];
+  },
+
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,POST,PUT,PATCH,DELETE,OPTIONS",
+          },
+          { key: "Access-Control-Allow-Headers", value: "*" },
+        ],
       },
     ];
   },
